0d1606acf1111896b97f094a7dbbb4f78b7b0459
Martin Sedlacek <martin.martin.sedlacek@gmail.com>
2024-05-30 22:52:40 +0200

    copy EDM consistency models repo

---
+++ src/consistency_model_edm/qm9/losses.py
@@ -0,0 +1,45 @@
+import torch
+
+
+def sum_except_batch(x):
+    return x.view(x.size(0), -1).sum(dim=-1)
+
+
+def assert_correctly_masked(variable, node_mask):
+    assert (variable * (1 - node_mask)).abs().sum().item() < 1e-8
+
+
+def compute_loss_and_nll(args, generative_model, nodes_dist, x, h, node_mask, edge_mask, context, generative_model_ema=None, N=None, boundaries=None):
+    bs, n_nodes, n_dims = x.size()
+
+    if args.probabilistic_model == 'diffusion':
+        edge_mask = edge_mask.view(bs, n_nodes * n_nodes)
+
+        assert_correctly_masked(x, node_mask)
+
+        # Here x is a position tensor, and h is a dictionary with keys
+        # 'categorical' and 'integer'.
+        if args.consistency:
+            if generative_model.training:
+                assert generative_model_ema is not None and N is not None and boundaries is not None
+            nll = generative_model(x, h, node_mask, edge_mask, context, generative_model_ema=generative_model_ema, N=N, boundaries=boundaries)
+        else:
+            nll = generative_model(x, h, node_mask, edge_mask, context)
+
+        N = node_mask.squeeze(2).sum(1).long()
+
+        log_pN = nodes_dist.log_prob(N)
+
+        assert nll.size() == log_pN.size()
+        nll = nll - log_pN
+
+        # Average over batch.
+        nll = nll.mean(0)
+
+        reg_term = torch.tensor([0.]).to(nll.device)
+        mean_abs_z = 0.
+    else:
+        raise ValueError(args.probabilistic_model)
+
+    return nll, reg_term, mean_abs_z
+
